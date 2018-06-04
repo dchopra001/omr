35740d181452e886bed7dd156bfe438ec6509c9e
Dhruv Chopra <Dhruv.C.Chopra@ibm.com>
2018-06-03 22:33:29 -0400

    Replace std::array for zOS compatibility

--- compiler/z/codegen/MyArray.hpp
+++ compiler/z/codegen/MyArray.hpp
@@ -1,16 +1,16 @@
-#include <array>
+#include "array.hpp"
 #include <cassert>
 #include <iostream>
 #include "infra/Assert.hpp"                        // for TR_ASSERT
 
 template <typename T, std::size_t N>
-struct my_array : std::array<T,N> {
+struct my_array : array<T,N> {
  T& operator[](std::size_t n) {
    TR_ASSERT(n < N, "Array index out of bounds!");
-   return (*static_cast<std::array<T,N>*>(this))[n];
+   return (*static_cast<array<T,N>*>(this))[n];
  }
  const T& operator[](std::size_t n) const {
    TR_ASSERT(n < N, "Array index out of bounds!");
-   return (*static_cast<const std::array<T,N>*>(this))[n];
+   return (*static_cast<const array<T,N>*>(this))[n];
  }
 };
