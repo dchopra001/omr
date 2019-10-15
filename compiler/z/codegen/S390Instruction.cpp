1c567e8ec83f05a9617fa44219c323bfb070b18a
Dhruv Chopra <Dhruv.C.Chopra@ibm.com>
2019-10-15 11:38:28 -0400

    Create TR_MethodCallAddress relocation for resolved methods
    Signed-off-by: Dhruv Chopra <Dhruv.C.Chopra@ibm.com>

--- compiler/z/codegen/S390Instruction.cpp
+++ compiler/z/codegen/S390Instruction.cpp
@@ -2477,10 +2477,18 @@ TR::S390RILInstruction::generateBinaryEncoding()
                }
 #endif
             (*(int32_t *) (cursor + 2)) = boi(i2);
+            TR::Symbol *sym = getSymbolReference()->getSymbol();
+            TR::ResolvedMethodSymbol *resolvedMethodSym = sym->getResolvedMethodSymbol();
+            TR_ResolvedMethod *resolvedMethod = resolvedMethodSym ? resolvedMethodSym->getResolvedMethod() : 0;
             if (getSymbolReference() && getSymbolReference()->getSymbol()->castToMethodSymbol()->isHelper())
                {
                AOTcgDiag1(comp, "add TR_HelperAddress cursor=%x\n", cursor);
                cg()->addProjectSpecializedRelocation(cursor+2, (uint8_t*) getSymbolReference(), NULL, TR_HelperAddress,
+                     __FILE__, __LINE__, getNode());
+               }
+            else if (resolvedMethod)
+               {
+               cg()->addProjectSpecializedRelocation(cursor+2, (uint8_t *)getSymbolReference()->getMethodAddress(), NULL, TR_MethodCallAddress,
                      __FILE__, __LINE__, getNode());
                }
             }
