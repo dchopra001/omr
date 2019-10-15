dcf2ce17fb29cb1243b545efa9a754187feaffc1
Dhruv Chopra <Dhruv.C.Chopra@ibm.com>
2019-10-15 11:16:48 -0400

    Don't add relocations for TR_ConstantPools in JITServer mode
    [skip ci]
    Signed-off-by: Dhruv Chopra <Dhruv.C.Chopra@ibm.com>

--- compiler/z/codegen/OMRTreeEvaluator.cpp
+++ compiler/z/codegen/OMRTreeEvaluator.cpp
@@ -6387,7 +6387,7 @@ OMR::Z::TreeEvaluator::checkAndSetMemRefDataSnippetRelocationType(TR::Node * nod
       int32_t reloType;
       if (node->getSymbol()->isDebugCounter())
          reloType = TR_DebugCounter;
-      else if (node->getSymbol()->isConst())
+      else if (node->getSymbol()->isConst() && !cg->comp()->isOutOfProcessCompilation())
          reloType = TR_ConstantPool;
       else if (node->getSymbol()->isClassObject())
          {
@@ -9556,7 +9556,7 @@ OMR::Z::TreeEvaluator::loadaddrEvaluator(TR::Node * node, TR::CodeGenerator * cg
                int32_t reloType;
                if (node->getSymbol()->isDebugCounter())
                   reloType = TR_DebugCounter;
-               else if (node->getSymbol()->isConst())
+               else if (node->getSymbol()->isConst() && !comp->isOutOfProcessCompilation())
                   reloType = TR_ConstantPool;
                else if (node->getSymbol()->isClassObject() && cg->needClassAndMethodPointerRelocations())
                   reloType = TR_ClassAddress;
